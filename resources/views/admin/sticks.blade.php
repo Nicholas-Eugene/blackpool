c137a3f35a804699a05e89c582e37d3aabfc0f76
S0lut <wijayavincent24@gmail.com>
2024-06-21 21:48:03 +0700

    booking and fnb crud in admin

--- resources/views/admin/sticks.blade.php
+++ resources/views/admin/sticks.blade.php
@@ -2,6 +2,7 @@
 
 @section('content')
     <h2>Sticks</h2>
+    <a href="{{ route('admin.createStick') }}" class="btn btn-primary">Add New Stick</a>
     <table class="table table-striped table-bordered">
         <thead>
             <tr>
@@ -11,6 +12,7 @@
                 <th>Description</th>
                 <th>Stock</th>
                 <th>Main Pic</th>
+                <th>Actions</th>
             </tr>
         </thead>
         <tbody>
@@ -21,9 +23,18 @@
                 <td>{{ $stick->price }}</td>
                 <td>{{ $stick->description }}</td>
                 <td>{{ $stick->stock }}</td>
-                <td><img src="{{asset('storage/img/stick/'. $stick->mainpic) }}" alt="Main Pic"></td>
+                <td><img src="{{ asset($stick->mainpic) }}" alt="Main Pic" style="width: 100px;"></td>
+                <td>
+                    <a href="{{ route('admin.editStick', $stick->id) }}" class="btn btn-primary">Edit</a>
+                    <form action="{{ route('admin.deleteStick', $stick->id) }}" method="POST" style="display:inline-block;">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="btn btn-danger">Delete</button>
+                    </form>
+                </td>
             </tr>
             @endforeach
         </tbody>
     </table>
 @endsection
+ 
